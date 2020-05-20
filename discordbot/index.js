a50ce67b1e7579bed394fc959f6359a6a93f8a83
SenorPez <senorpez@senorpez.com>
2020-05-20 15:32:13 +0000

    Working on bot commands for charges.

--- discordbot/index.js
+++ discordbot/index.js
@@ -44,6 +44,8 @@ function processCommand(receivedMessage) {
     playerId = item
     receivedMessage.channel.send(`Player Id set to ${playerId}`)
   }
+  if (command == "!setcharges") {
+    setCharges(receivedMessage, quantity, item, args)
 }
 
 function dropItem(receivedMessage, quantity, item, args) {
@@ -64,13 +66,105 @@ function addItem(receivedMessage, quantity, item, args) {
   jsonAddItem(receivedMessage, quantity, item, args)
 }
 
+function setCharges(receivedMessage, quantity, item, args) {
+  if (campaignId == null || playerId == null) {
+    receivedMessage.channel.send("Campaign or player ID not set. Set with !cid and !pid.")
+    return
+  }
+
+  fetch("http://senorpez.com:9090/items")
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      const itemArray = data._embedded['loottable-api:lootitem']
+      const filteredItemArray = itemArray.filter(singleItem => singleItem.name == item)
+
+      const idRegEx = /(--[i]) ?(.+?(?=--|$))/g
+      const idMatches = [...args.matchAll(idRegEx)]
+
+      if (filteredItemArray.length < 1) {
+        receivedMessage.channel.send("Item not found.")
+      } else if (filteredItemArray.length > 1 && idMatches.length < 1) {
+        receivedMessage.channel.send("Multiple item matches. Include item ID with --i option.")
+      } else if (filteredItemArray.length > 1 && idMatches.length >= 1) {
+        const itemId = idMatches[0][2]
+        const remarkRegEx = /(--[r]) ?(.+?(?=--|$))/g
+        const remarkMatches = [...args.matchAll(remarkRegEx)]
+        let remark = null
+        if (remarkMatches.length > 0) {
+          remark = remarkMatches[0][2]
+        }
+
+        // A zero-quantity transaction for charge changes.
+        let addTransaction = {item: itemId, quantity: 0, remark: remark}
+
+        fetch(`http://senorpez.com:9090/campaigns/${campaignId}/players/${playerId}/itemtransactions`,
+          {method: "post", body: JSON.stringify(addTransaction), headers: {
+            "Content-Type": "application/hal+json"
+          }})
+        .then(function(response) {
+          return response.json()
+        })
+        .then(data => {
+          console.log(data)
+        })
+
+        let setCharges = {charges: quantity}
+        fetch(`http://senorpez.com:9090/items/${itemId}`,
+          {method: "put", body: JSON.stringify(setCharges), headers: {
+            "Content-Type": "application/hal+json"
+          }})
+        .then(function(response) {
+          return response.json()
+        })
+        .then(data => {
+          console.log(data)
+        })
+      } else {
+        const itemId = filteredItemArray[0].id
+
+        const remarkRegEx = /(--[r]) ?(.+?(?=--|$))/g
+        const remarkMatches = [...args.matchAll(remarkRegEx)]
+        let remark = null
+        if (remarkMatches.length > 0) {
+          remark = remarkMatches[0][2]
+        }
+
+        // A zero-quantity transaction for charge changes.
+        let addTransaction = {item: itemId, quantity: 0, remark: remark}
+
+        fetch(`http://senorpez.com:9090/campaigns/${campaignId}/players/${playerId}/itemtransactions`,
+          {method: "post", body: JSON.stringify(addTransaction), headers: {
+            "Content-Type": "application/hal+json"
+          }})
+        .then(function(response) {
+          return response.json()
+        })
+        .then(data => {
+          console.log(data)
+        })
+
+        let setCharges = {charges: quantity}
+        fetch(`http://senorpez.com:9090/items/${itemId}`,
+          {method: "put", body: JSON.stringify(setCharges), headers: {
+            "Content-Type": "application/hal+json"
+          }})
+        .then(function(response) {
+          return response.json()
+        })
+        .then(data => {
+          console.log(data)
+        })
+      }
+    })
+}
+
 function jsonAddItem(receivedMessage, quantity, item, args) {
   if (campaignId === null || playerId === null) {
     receivedMessage.channel.send("Campaign or player ID not set. Set with !cid and !pid.")
     return
   }
-
-  console.log(playerId)
 
   fetch("http://senorpez.com:9090/items")
     .then(response => {
