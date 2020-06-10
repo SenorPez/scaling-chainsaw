3312dc739b07bfabec8a029804b76baed3c507c4
SenorPez <senorpez@senorpez.com>
2020-06-10 13:13:51 -0500

    Update to find campaign by name.

--- discordbot/commands/campaign.js
+++ discordbot/commands/campaign.js
@@ -1,21 +1,37 @@
-const regex = /^.+(?:\s)+(\d+)/g
+const regex = /^.+?(?:\s)+(.+)/g
 const fetch = require("node-fetch");
 
 module.exports = (message, state) => {
     const matches = [...message.content.matchAll(regex)];
     if (matches[0]) {
         const campaignId = Number(matches[0][1]).valueOf();
-
-        findCampaign(campaignId)
-            .then(data => {
-                if (data.length < 1) {
-                    message.channel.send(`Campaign with id of ${campaignId} not found.`);
-                } else if (data.length > 1) {
-                    message.channel.send(`Seriously, how did you manage this?`);
-                }
-                message.channel.send(`Campaign set to ${data[0].name}`);
-                state.campaignId = campaignId;
-            });
+        if (isNaN(campaignId)) {
+            findCampaignByName(matches[0][1])
+                .then(data => {
+                    if (data.length < 1) {
+                        message.channel.send(`Campaign with name of ${matches[0][1]} not found.`);
+                    } else if (data.length > 1) {
+                        message.channel.send(`Multiple matches`);
+                    } else {
+                        message.channel.send(`Campaign set to ${data[0].name}`);
+                        state.campaignId = data[0].id;
+                        console.log("Campaign", state);
+                    }
+                })
+        } else {
+            findCampaignById(campaignId)
+                .then(data => {
+                    if (data.length < 1) {
+                        message.channel.send(`Campaign with id of ${campaignId} not found.`);
+                    } else if (data.length > 1) {
+                        message.channel.send(`Seriously, how did you manage this?`);
+                    } else {
+                        message.channel.send(`Campaign set to ${data[0].name}`);
+                        console.log(campaignId);
+                        state.campaignId = campaignId;
+                    }
+                });
+        }
     } else {
         message.channel.send("Campaign data must be an integer.");
     }
@@ -26,8 +42,14 @@ function getCampaigns() {
         .then(response => response.json());
 }
 
-function findCampaign(campaignId) {
+function findCampaignById(campaignId) {
     return getCampaigns().then(data => {
         return data._embedded['loot-api:campaign'].filter(campaign => campaign.id === campaignId);
     });
+}
+
+function findCampaignByName(campaignName) {
+    return getCampaigns().then(data => {
+        return data._embedded['loot-api:campaign'].filter(campaign => campaign.name.toLowerCase().includes(campaignName.toLowerCase()));
+    })
 }
--- discordbot/events/message.js
+++ discordbot/events/message.js
@@ -10,13 +10,15 @@ module.exports = (client, state, message) => {
         return;
     }
 
+    console.log("Message S", state);
     if (message.content.startsWith("$")) {
         const command = message.content.split(" ")[0].slice(1).toString();
         const matchedCommand = Object.keys(alias).filter(key => key === command);
         if (matchedCommand.length < 1) {
             message.channel.send("No command matched. Type $help for a list of commands");
         } else {
-            alias[matchedCommand[0]](message, state);
+            console.log(alias[matchedCommand[0]](message, state));
         }
     }
+    console.log("Message", state);
 }
