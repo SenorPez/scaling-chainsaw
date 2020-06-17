44084ee0e266c8ccbc3d44c0b3ec1883e4791642
SenorPez <senorpez@senorpez.com>
2020-06-16 19:54:54 -0500

    Tests against local API for campaign

--- discordbot/test/campaign.test.js
+++ discordbot/test/campaign.test.js
@@ -322,6 +322,267 @@ suite('Mock API', function () {
     });
 });
 
+suite('Local API', function () {
+    setup(function () {
+        state.setCampaignId(null);
+        state.setCharacterId(null);
+    });
+
+    teardown(function () {
+        state.setCampaignId(null);
+        state.setCharacterId(null);
+    });
+
+    test('parseMessage: Regex match, valid command', function () {
+        const mockMessage = {content: '$campaign Defiance in Phlan'};
+        return parseMessage(mockMessage)
+            .then(response => assert.isArray(response));
+    });
+
+    test('parseMessage: No regex match, invalid command', function () {
+        const mockMessage = {content: 'fail'}
+        return parseMessage(mockMessage)
+            .then(() => assert.fail())
+            .catch(error => assert.strictEqual(error.message, "Usage: $campaign <campaign name>"));
+    });
+
+    test('parseArguments: Search argument is text', function () {
+        const mockMatches = [null, 'Search'];
+        return parseArguments(mockMatches)
+            .then(textId => assert.strictEqual(textId, 'Search'),
+                () => assert.fail());
+    });
+
+    test('parseArguments: Search argument is number', function () {
+        const mockMatches = [null, '8675309'];
+        return parseArguments(mockMatches)
+            .then(() => assert.fail(),
+                (numberId) => assert.strictEqual(numberId, 8675309));
+    });
+
+    test('getCampaign: Valid Campaigns JSON', function () {
+        const {getCampaigns} = require('../commands/campaign');
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+        process.env.API_URL = 'https://localhost:9090';
+
+        return getCampaigns()
+            .then(response => response.json())
+            .then(campaigns => {
+                assert.hasAllKeys(campaigns, ['_embedded', '_links']);
+                assert.hasAllKeys(campaigns._embedded, 'loot-api:campaign');
+                campaigns._embedded["loot-api:campaign"].forEach(campaign => {
+                    assert.hasAllKeys(campaign, ['id', 'name', '_links']);
+                    assert.hasAllKeys(campaign._links, 'self');
+                    assert.hasAllKeys(campaign._links.self, 'href');
+                });
+                assert.hasAllKeys(campaigns._links, ['self', 'index', 'curies']);
+                assert.hasAllKeys(campaigns._links.self, 'href');
+                assert.hasAllKeys(campaigns._links.index, 'href');
+                assert.hasAllKeys(campaigns._links.curies[0], ['href', 'name', 'templated']);
+            });
+    });
+
+    test('findCampaignByName: Valid Campaign JSON, matched by exact name', function () {
+        const {findCampaignByName} = require('../commands/campaign');
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+        process.env.API_URL = 'https://localhost:9090';
+
+        return findCampaignByName('Defiance in Phlan')
+            .then(response => response.json())
+            .then(campaign => {
+                assert.hasAllKeys(campaign, ['id', 'name', '_links']);
+                assert.hasAllKeys(campaign._links, [
+                    'self',
+                    'loot-api:campaigns',
+                    'loot-api:characters',
+                    'index',
+                    'curies'
+                ]);
+                assert.hasAllKeys(campaign._links.self, 'href');
+                assert.hasAllKeys(campaign._links.index, 'href');
+                assert.hasAllKeys(campaign._links["loot-api:campaigns"], 'href');
+                assert.hasAllKeys(campaign._links["loot-api:characters"], 'href');
+                assert.hasAllKeys(campaign._links.curies[0], ['href', 'name', 'templated']);
+            });
+    });
+
+    test('findCampaignByName: Valid Campaign JSON, matched by differently cased name', function () {
+        const {findCampaignByName} = require('../commands/campaign');
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+        process.env.API_URL = 'https://localhost:9090';
+
+        return findCampaignByName('dEFiAnCE iN pHLAn')
+            .then(response => response.json())
+            .then(campaign => {
+                assert.hasAllKeys(campaign, ['id', 'name', '_links']);
+                assert.hasAllKeys(campaign._links, [
+                    'self',
+                    'loot-api:campaigns',
+                    'loot-api:characters',
+                    'index',
+                    'curies'
+                ]);
+                assert.hasAllKeys(campaign._links.self, 'href');
+                assert.hasAllKeys(campaign._links.index, 'href');
+                assert.hasAllKeys(campaign._links["loot-api:campaigns"], 'href');
+                assert.hasAllKeys(campaign._links["loot-api:characters"], 'href');
+                assert.hasAllKeys(campaign._links.curies[0], ['href', 'name', 'templated']);
+            });
+    });
+
+    test('findCampaignByName: Valid Campaign JSON, matched by partial name', function () {
+        const {findCampaignByName} = require('../commands/campaign');
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+        process.env.API_URL = 'https://localhost:9090';
+
+        return findCampaignByName('Phlan')
+            .then(response => response.json())
+            .then(campaign => {
+                assert.hasAllKeys(campaign, ['id', 'name', '_links']);
+                assert.hasAllKeys(campaign._links, [
+                    'self',
+                    'loot-api:campaigns',
+                    'loot-api:characters',
+                    'index',
+                    'curies'
+                ]);
+                assert.hasAllKeys(campaign._links.self, 'href');
+                assert.hasAllKeys(campaign._links.index, 'href');
+                assert.hasAllKeys(campaign._links["loot-api:campaigns"], 'href');
+                assert.hasAllKeys(campaign._links["loot-api:characters"], 'href');
+                assert.hasAllKeys(campaign._links.curies[0], ['href', 'name', 'templated']);
+            });
+    });
+
+    test('findCampaignByName: No text match', function () {
+        const {findCampaignByName} = require('../commands/campaign');
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+        process.env.API_URL = 'https://localhost:9090';
+
+        return findCampaignByName('leeadamaisfat')
+            .then(() => assert.fail())
+            .catch((error) => assert.strictEqual(error.message, 'Campaign containing \'leeadamaisfat\' not found'));
+    });
+
+    test('findCampaignByName: Multiple text matches', function () {
+        const {findCampaignByName} = require('../commands/campaign');
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+        process.env.API_URL = 'https://localhost:9090';
+
+        return findCampaignByName('p')
+            .then(() => assert.fail())
+            .catch((error) => assert.strictEqual(error.message, 'Multiple campaigns containing \'p\' found:\nID: 1 Name: Defiance in Phlan\nID: 3 Name: New Campaign'));
+    });
+
+    test('findCampaignById: Valid Campaign JSON, matched by ID', function () {
+        const {findCampaignById} = require('../commands/campaign');
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+        process.env.API_URL = 'https://localhost:9090';
+
+        return findCampaignById(1)
+            .then(response => response.json())
+            .then(campaign => {
+                assert.hasAllKeys(campaign, ['id', 'name', '_links']);
+                assert.hasAllKeys(campaign._links, [
+                    'self',
+                    'loot-api:campaigns',
+                    'loot-api:characters',
+                    'index',
+                    'curies'
+                ]);
+                assert.hasAllKeys(campaign._links.self, 'href');
+                assert.hasAllKeys(campaign._links.index, 'href');
+                assert.hasAllKeys(campaign._links["loot-api:campaigns"], 'href');
+                assert.hasAllKeys(campaign._links["loot-api:characters"], 'href');
+                assert.hasAllKeys(campaign._links.curies[0], ['href', 'name', 'templated']);
+            });
+    });
+
+    test('findCampaignById: No ID match', function () {
+        const {findCampaignById} = require('../commands/campaign');
+        process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+        process.env.API_URL = 'https://localhost:9090';
+
+        return findCampaignById(8675309)
+            .then(() => assert.fail())
+            .catch(error => assert.strictEqual(error.message, "Campaign with ID of 8675309 not found"));
+    });
+
+    test('setCampaign: No character reset', function () {
+        const mockCampaign = {
+            id: 1,
+            name: "Defiance in Phlan",
+            _links: {
+                self: {href: "https://localhost:9090/campaigns/1"},
+                'loot-api:campaigns': {href: "https://localhost:9090/campaigns"},
+                'loot-api:characters': {href: "https://localhost:9090/campaigns/1/characters"},
+                index: {href: "https://localhost:9090"},
+                curies: [
+                    {
+                        href: "https://localhost:9090/docs/reference.html#resources-loot-{rel}",
+                        name: "loot-api",
+                        templated: true
+                    }
+                ]
+            }
+        };
+        const mockJson = sinon.stub().returns(Promise.resolve(mockCampaign));
+        const mockResponse = {json: mockJson};
+
+        const mockSend = sinon.stub();
+        const mockMessage = {
+            channel: {send: mockSend}
+        };
+
+        state.setCampaignId(1);
+        state.setCharacterId(8675309);
+
+        return setCampaign(mockResponse, mockMessage)
+            .then(() => {
+                assert.strictEqual(state.getCampaignId(), mockCampaign.id);
+                assert.strictEqual(state.getCharacterId(), 8675309);
+            });
+    });
+
+    test('setCampaign: Character reset', function () {
+        const mockCampaign = {
+            id: 1,
+            name: "Defiance in Phlan",
+            _links: {
+                self: {href: "https://localhost:9090/campaigns/1"},
+                'loot-api:campaigns': {href: "https://localhost:9090/campaigns"},
+                'loot-api:characters': {href: "https://localhost:9090/campaigns/1/characters"},
+                index: {href: "https://localhost:9090"},
+                curies: [
+                    {
+                        href: "https://localhost:9090/docs/reference.html#resources-loot-{rel}",
+                        name: "loot-api",
+                        templated: true
+                    }
+                ]
+            }
+        };
+        const mockJson = sinon.stub().returns(Promise.resolve(mockCampaign));
+        const mockResponse = {json: mockJson};
+
+        const mockSend = sinon.stub();
+        const mockMessage = {
+            channel: {send: mockSend}
+        };
+
+        state.setCampaignId(2);
+        state.setCharacterId(8675309);
+
+        return setCampaign(mockResponse, mockMessage)
+            .then(() => {
+                assert.strictEqual(state.getCampaignId(), mockCampaign.id);
+                assert.isNull(state.getCharacterId());
+            });
+    });
+})
+
+
+
 // suite('Reference API', function () {
 //     test('Should return valid Campaigns JSON', function () {
 //         return getCampaigns()
