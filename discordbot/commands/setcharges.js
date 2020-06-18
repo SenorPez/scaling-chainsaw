2657dae23010097d91291459b3245833560fd2cd
SenorPez <senorpez@senorpez.com>
2020-06-17 20:23:21 -0500

    Commenting out set charges; need to rearchitecture.

--- discordbot/commands/setcharges.js
+++ discordbot/commands/setcharges.js
@@ -4,39 +4,41 @@ const fetch = require("node-fetch");
 const state = require('../service/state');
 
 module.exports = (message) => {
-    const tokenPromise = getToken();
-    const itemPromise = parseMessage(message)
-        .then(matches => parseCommand(matches))
-        .then(
-            searchParam => findItemByName(searchParam),
-            searchParam => findItemById(searchParam)
-        )
-        .then(result => result.json())
-        .catch(error => {
-            message.channel.send(error.message);
-            throw error;
-        });
-    const argsPromise = parseMessage(message)
-        .then(matches => parseArguments(matches))
-        .catch(error => {
-            message.channel.send(error.message);
-            throw error;
-        });
-
-    return Promise.all([tokenPromise, itemPromise, argsPromise])
-        .then(values => {
-            const item = values[1];
-            const arguments = values[2];
-            const token = values[0];
-            return module.exports.postCharges(message, item, arguments, token);
-        })
-        .catch(error => error);
+    return Promise.resolve();
+    // const tokenPromise = getToken();
+    // const itemPromise = parseMessage(message)
+    //     .then(matches => parseCommand(matches))
+    //     .then(searchParam => {
+    //         return (typeof searchParam === 'number')
+    //             ? findItemById(searchParam)
+    //             : findItemByName(searchParam);
+    //     })
+    //     .then(result => result.json())
+    //     .catch(error => {
+    //         message.channel.send(error.message);
+    //         throw error;
+    //     });
+    // const argsPromise = parseMessage(message)
+    //     .then(matches => parseArguments(matches))
+    //     .catch(error => {
+    //         message.channel.send(error.message);
+    //         throw error;
+    //     });
+    //
+    // return Promise.all([tokenPromise, itemPromise, argsPromise])
+    //     .then(values => {
+    //         const item = values[1];
+    //         const arguments = values[2];
+    //         const token = values[0];
+    //         return module.exports.postCharges(message, item, arguments, token);
+    //     })
+    //     .catch(error => error);
 };
 
 module.exports.postCharges = (message, item, arguments, token) => {
     const remark = arguments.arguments.r === null
         ? `Set charges to ${arguments.quantity}`
-        : arguments.arguments.r + ` (Set charnges to ${arguments.quantity}`;
+        : arguments.arguments.r + ` (Set charges to ${arguments.quantity}`;
     const newTransaction = {
         item: item.id,
         remark: remark
