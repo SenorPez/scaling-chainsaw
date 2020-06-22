f5f3ee6dc989621635af150b33f143bfee927028
SenorPez <senorpez@senorpez.com>
2020-06-22 13:48:54 -0500

    Tests for findItemOnCharacter

--- discordbot/test/dropitem.spec.js
+++ discordbot/test/dropitem.spec.js
@@ -43,6 +43,7 @@ const mockCharacter = {
     name: 'Vorgansharanx',
     inventory: [
         {
+            id: 1,
             name: 'Gold Piece',
             quantity: 420
         }
@@ -382,6 +383,65 @@ suite('Mock API', function() {
         return findItemById(8675309)
             .then(() => assert.fail())
             .catch(error => assert.strictEqual(error.message, "Item with ID of 8675309 not found"));
+    });
+
+    test('findItemOnCharacter: Item found, sufficient quantity', function () {
+        const proxyquire = require('proxyquire');
+        const fetchMock = require('fetch-mock').sandbox();
+        fetchMock.mock(
+            'http://mockserver/',
+            mockIndex
+        );
+        fetchMock.mock(
+            'http://mockserver/campaigns/6/characters/6/',
+            mockCharacter
+        );
+        const api = proxyquire('..service/api', {'node-fetch': fetchMock});
+        const {findItemOnCharacter} = proxyquire('../commands/dropitem', {'../service/api': api});
+
+        return findItemOnCharacter(mockItem, 1)
+            .then(item => {
+                assert.hasAllKeys(item, ['id', 'name']);
+                assert.isOk(fetchMock.done());
+            });
+    });
+
+    test('findItemOnCharacter: Item found, insufficient quantity', function () {
+        const proxyquire = require('proxyquire');
+        const fetchMock = require('fetch-mock').sandbox();
+        fetchMock.mock(
+            'http://mockserver/',
+            mockIndex
+        );
+        fetchMock.mock(
+            'http://mockserver/campaigns/6/characters/6/',
+            mockCharacter
+        );
+        const api = proxyquire('..service/api', {'node-fetch': fetchMock});
+        const {findItemOnCharacter} = proxyquire('../commands/dropitem', {'../service/api': api});
+
+        return findItemOnCharacter(mockItem, 1000000)
+            .then(() => assert.fail())
+            .catch(error => assert.strictEqual(error.message, "Cannot drop 1000000 Gold Piece; Aethelwuf has 420 Gold Piece"));
+    });
+
+    test('findItemOnCharacter: Item not found', function () {
+        const proxyquire = require('proxyquire');
+        const fetchMock = require('fetch-mock').sandbox();
+        fetchMock.mock(
+            'http://mockserver/',
+            mockIndex
+        );
+        fetchMock.mock(
+            'http://mockserver/campaigns/6/characters/6/',
+            mockCharacter
+        );
+        const api = proxyquire('..service/api', {'node-fetch': fetchMock});
+        const {findItemOnCharacter} = proxyquire('../commands/dropitem', {'../service/api': api});
+
+        return findItemOnCharacter({id: 8675039, name: 'Jenny'}, 1)
+            .then(() => assert.fail())
+            .catch(error => assert.strictEqual(error.message, "Cannot drop 1 Jenny; Aethelwuf has 0 Jenny"));
     });
 
     test('postTransaction: Valid updated JSON', function () {
