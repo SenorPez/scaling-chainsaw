1a64766b65427730e95d620edde0eaa4853ace08
SenorPez <senorpez@senorpez.com>
2020-06-23 10:52:10 -0500

    Setup of local data source.

--- api/src/main/java/com/senorpez/loot/api/ItemTransactionRepository.java
+++ api/src/main/java/com/senorpez/loot/api/ItemTransactionRepository.java
@@ -7,11 +7,11 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 interface ItemTransactionRepository extends CrudRepository<ItemTransaction, Integer> {
-    @Query(value = "SELECT item_id, SUM(quantity) AS quantity, name, weight, details, charges " +
-            "FROM itemtransactions " +
-            "LEFT JOIN items ON items.id = itemtransactions.item_id " +
-            "WHERE character_id = :characterId AND campaign_id = :campaignId " +
-            "GROUP BY item_id " +
-            "HAVING SUM(quantity) <> 0", nativeQuery = true)
+    @Query(value = "SELECT `item_id`, SUM(`quantity`) AS quantity, `name`, `weight`, `details`, `charges` " +
+            "FROM `itemtransactions` " +
+            "LEFT JOIN `items` ON `items`.`id` = `itemtransactions`.`item_id` " +
+            "WHERE `character_id` = :characterId AND `campaign_id` = :campaignId " +
+            "GROUP BY `item_id` " +
+            "HAVING SUM(`quantity`) <> 0", nativeQuery = true)
     List<Object[]> getInventory(@Param("characterId") int characterId, @Param("campaignId") int campaignId);
 }
