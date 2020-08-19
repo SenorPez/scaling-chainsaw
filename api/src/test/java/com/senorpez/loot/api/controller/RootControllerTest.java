da571c68e77c3f71e752192ef7cb5f7ce16504e3
SenorPez <senorpez@senorpez.com>
2020-08-19 17:59:10 -0500

    More testing and documentation for RootController.

--- api/src/test/java/com/senorpez/loot/api/controller/RootControllerTest.java
+++ api/src/test/java/com/senorpez/loot/api/controller/RootControllerTest.java
@@ -2,16 +2,24 @@ package com.senorpez.loot.api.controller;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.springframework.hateoas.MediaTypes.HAL_JSON;
+import static org.springframework.hateoas.MediaTypes.HAL_JSON_VALUE;
+import static org.springframework.restdocs.headers.HeaderDocumentation.*;
+import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
+import static org.springframework.restdocs.snippet.Attributes.key;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = RootController.class)
+@AutoConfigureRestDocs
 class RootControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -23,6 +31,25 @@ class RootControllerTest {
         mockMvc.perform(get("/").accept(HAL_JSON))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(HAL_JSON))
-                .andExpect(content().string(matchesJsonSchemaInClasspath(OBJECT_SCHEMA)));
+                .andExpect(content().string(matchesJsonSchemaInClasspath(OBJECT_SCHEMA)))
+                .andDo(document(
+                        "index",
+                        preprocessRequest(prettyPrint()),
+                        preprocessResponse(prettyPrint()),
+                        requestHeaders(
+                                headerWithName("Accept")
+                                        .description("Accept header.")
+                                        .attributes(key("accept").value(HAL_JSON_VALUE))
+                        ),
+                        responseHeaders(
+                                headerWithName("Content-Type")
+                                        .description("Content-Type header.")
+                                        .attributes(key("contenttype").value(HAL_JSON_VALUE))
+                        ),
+                        links(
+                                halLinks(),
+                                linkWithRel("self").description("This resource.")
+                        )
+                ));
     }
 }
