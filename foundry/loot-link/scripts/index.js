6614e90bce119c8f7f721f20b3d33dfeb58fe9d5
SenorPez <senorpez@senorpez.com>
2021-06-03 11:11:48 -0500

    Created system skeleton.

--- foundry/loot-link/scripts/index.js
+++ foundry/loot-link/scripts/index.js
@@ -3,35 +3,68 @@ const modName = "Loot Link";
 
 const Url = 'http://localhost:9090/foundryitems/';
 
-async function logItem(wrapped, ...args) {
-    const items = args[0];
-    const actor = args[1].parent;
+async function addItem(wrapped, ...args) {
+    args[1] = [args[1][0], args[1][0]];
+    const actorId = this.id;
 
-    const apiData = {
-        actorId: actor.id,
-        item: items[0]
+    for (const item of args[1]) {
+        const apiData = {
+            actorId: actorId,
+            item: item
+        };
+
+        await fetch(Url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(apiData)
+        })
+            .then(response => {
+                if (!response.ok) throw new Error("Fetch Error");
+                return response.json();
+            })
+            .then(data => {
+                console.log(modId + " | Created item with ID " + data.id);
+                return data.id;
+            })
+            .then(id =>
+                wrapped(args[0], [item])
+                    .then(newItems => newItems.forEach(newItem => newItem.setFlag(modId, "id", id))))
+            .catch(error => console.error("Error: " + error));
     }
-
-    await fetch(Url, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(apiData)
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log(modId + " | Created item with ID " + data.id);
-            return data.id;
-        })
-        .then(id => {
-            wrapped(...args)
-                .then(newItems => newItems.forEach(item => item.setFlag(modId, "id", id)));
-        })
-        .catch(error => {
-            console.error("Error: " + error);
-        });
 }
+//
+// async function deleteItem(wrapped, ...args) {
+//     const actor = args[1].parent;
+//     const itemFoundryId = args[0][0];
+//     const itemId = actor.getEmbeddedCollection("Item").get(itemFoundryId).getFlag(modId, "id");
+//
+//     console.log(itemId);
+//
+//     const apiData = {
+//         id: itemId
+//     };
+//
+//     await fetch(Url, {
+//         method: 'DELETE',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(apiData)
+//     })
+//         .then(response => {
+//             if (!response.ok) throw new Error('Error recieved');
+//             return response.json();
+//         })
+//         .then(data => {
+//             console.log(data);
+//             console.log(modId + " | Deleted item with ID " + data.id);
+//             return data.id;
+//         })
+//         .then(id => console.log("Run wrapped"))
+//         .catch(error => console.error("Error: " + error));
+// }
 
 Hooks.once("init", () => {
     console.log(modName + " | Initializing")
@@ -43,5 +76,6 @@ Hooks.once("ready", () => {
         console.log(modName + " | libwrapper Not Found");
     }
 
-    libWrapper.register(modId, 'Item.createDocuments', logItem, 'MIXED');
+    libWrapper.register(modId, 'Actor.prototype.createEmbeddedDocuments', addItem, 'MIXED');
+    // libWrapper.register(modId, 'Item.deleteDocuments', deleteItem, 'MIXED');
 })
---
+++ system/README.md
@@ -0,0 +1,3 @@
+# Test System Please Ignore
+
+Windmills ahoy.
